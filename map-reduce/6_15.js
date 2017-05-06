a5a29a1fe8c0ab7c24cf486154b7261154eda5b1
Michał Cywiński <mcywinski@hotmail.com>
2017-05-06 10:30:44 +0200

    Poprawka map-reduce przykład 15.

--- map-reduce/6_15.js
+++ map-reduce/6_15.js
@@ -1,16 +1,16 @@
-var map = function() {
-    if (this.nationality !== "Poland" || this.sex !== "Female")
-        return;
-    
-    for (var i = 0; i < this.credit.length; i++)
-        emit(this.credit[i].currency, this.credit[i].balance);
-};
-
-var reduce = function(key, values) {
-    var sum = 0;
-    for (var i = 1; i < values.length; i++) sum += values[i];
-    return { summed_balance: sum, avg_balance: sum / (values.length - 1) };
-};
-
-db.people.mapReduce(map, reduce, { out: 'poeple_female_pol_currency_remainings' });
+var map = function() {
+    if (this.nationality !== "Poland" || this.sex !== "Female")
+        return;
+    
+    for (var i = 0; i < this.credit.length; i++)
+        emit(this.credit[i].currency, this.credit[i].balance);
+};
+
+var reduce = function(key, values) {
+    var sum = 0;
+    for (var i = 0; i < values.length; i++) sum += values[i];
+    return { summed_balance: sum, avg_balance: sum / values.length };
+};
+
+db.people.mapReduce(map, reduce, { out: 'poeple_female_pol_currency_remainings' });
 db.poeple_female_pol_currency_remainings.find({})
